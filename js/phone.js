da78ee6f76486686281fdd297c8c6a0678444c89
mrana007 <rana.es.bc@gmail.com>
2023-08-29 02:01:31 +0600

    added show all button

--- js/phone.js
+++ js/phone.js
@@ -6,24 +6,37 @@ const loadPhone = async (searchText) => {
     displayPhones(phones);
 }
 
+
 const displayPhones = phones =>{
-    // console.log(phones);
+    console.log(phones);
 
     const phoneContainer = document.getElementById('phone-container');
     // clear phone container card before adding new cards
     phoneContainer.textContent = '';
 
+    // display show all phone button if there are more then 12 phones
+    const showAllContainer = document.getElementById('show-all-container')
+    if(phones.length > 12){
+        showAllContainer.classList.remove('hidden');
+    }
+    else{
+        showAllContainer.classList.add('hidden')
+    }
+
+    // display only first 12 phones
+    phones = phones.slice(0, 12);
+
     phones.forEach(phone => {
-        console.log(phone);
+        // console.log(phone);
         // step-2: create a div
         const phoneCard = document.createElement('div');
         phoneCard.classList = `card bg-gray-100 p-4 shadow-xl`;
         // step-3: set innerHTML
         phoneCard.innerHTML = `
-        <figure><img src="${phone.image}" alt="Shoes" /></figure>
+        <figure><img src="${phone.image}" alt="Phones" /></figure>
         <div class="card-body">
             <h2 class="card-title">${phone.phone_name}</h2>
-            <p>If a dog chews shoes whose shoes does he choose?</p>
+            <p>There are many variations of passages of available, but the majority have suffered</p>
             <div class="card-actions justify-end">
                 <button class="btn btn-primary">Buy Now</button>
             </div>
@@ -41,6 +54,13 @@ const handleSearch = () =>{
     const searchText = searchField.value;
     console.log(searchText);
     loadPhone(searchText);
+}
+
+const handleSearch2 = () =>{
+    const searchField = document.getElementById('search-field2');
+    const searchText = searchField.value;
+    loadPhone(searchText);
+
 }
 
 // loadPhone();
