dde82be619f5139201d3bd2c24266f0118c926a7
mrana007 <rana.es.bc@gmail.com>
2023-08-29 04:39:49 +0600

    added information of show details modal

--- js/phone.js
+++ js/phone.js
@@ -8,7 +8,7 @@ const loadPhone = async (searchText, isShowAll) => {
 
 
 const displayPhones = (phones, isShowAll) =>{
-    console.log(phones);
+    // console.log(phones);
 
     const phoneContainer = document.getElementById('phone-container');
     // clear phone container card before adding new cards
@@ -22,7 +22,7 @@ const displayPhones = (phones, isShowAll) =>{
     else{
         showAllContainer.classList.add('hidden')
     }
-    console.log('is show all', isShowAll);
+    // console.log('is show all', isShowAll);
     // display only first 12 phones if not show All
     if(!isShowAll){
         phones = phones.slice(0, 12);
@@ -39,8 +39,8 @@ const displayPhones = (phones, isShowAll) =>{
         <div class="card-body">
             <h2 class="card-title">${phone.phone_name}</h2>
             <p>There are many variations of passages of available, but the majority have suffered</p>
-            <div class="card-actions justify-end">
-                <button class="btn btn-primary">Buy Now</button>
+            <div class="card-actions justify-center">
+                <button onclick = "handleShowDetails('${phone.slug}')" class="btn btn-primary">Show Details</button>
             </div>
         </div>
         `;
@@ -49,6 +49,32 @@ const displayPhones = (phones, isShowAll) =>{
     })
     // hide loading spinner
     toggleLoadingSpinner(false);
+}
+
+// 
+const handleShowDetails = async (id) =>{
+    // console.log('click show details', id);
+    // load single phone data
+    const res = await fetch(`https://openapi.programming-hero.com/api/phone/${id}`);
+    const data = await res.json();
+    const phone = data.data;
+    showPhoneDetails(phone);
+}
+
+const showPhoneDetails = (phone) =>{
+    console.log(phone);
+    const phoneName = document.getElementById('show-details-phone-name');
+    phoneName.innerText = phone.name;
+
+    const showDetailsContainer = document.getElementById('show-details-container');
+    showDetailsContainer.innerHTML = `
+        <img src="${phone.image}" alt"" />
+        <p><span>Storage</span>${phone?.mainFeatures?.storage}</p>
+        <p><span>Storage</span>${phone?.others?.GPS}</p>
+    `
+
+    // show the modal
+    show_details_modal.showModal()
 }
  
 
